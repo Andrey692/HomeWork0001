49b947582fac772e876b44f8a713c34942aa16f3
Andrey <bot6923777@outlook.com>
2023-11-30 12:41:05 +0300

    сохранение1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задайте массив на 10 целых чисел.
+// Определить колличество четных чисел в массиве.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(1,10); 
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+            System.Console.Write("["+ string.Join(", ",collection)+"] " + "=> ");
+
+}
+
+
+int  IndexOf(int[] collection)
+{
+    int count = collection.Length;
+    int index = 0;
+    int scor = 0;   // подсчет четных элементов
+
+    while (index<count)
+    {
+        if (collection[index] % 2 == 0)
+        {
+            scor = scor + 1;
+        }
+        index++;
+    }
+    return scor;
+}
+
+int[] array = new int[10];  // создаем новый массив в котором 10 элементов
+
+FillArray(array);  // вызов функции заполнения
+
+PrintArray(array); // вызов функции вывода
+
+
+int pos = IndexOf(array);
+System.Console.WriteLine(pos);
